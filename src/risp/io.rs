2760bee3f5019ee8f78feb26cc07a67c6c873f86
Armando Herrera <arherrer@intra.42.fr>
2019-06-24 17:33:23 -0700

    fix

--- src/risp/io.rs
+++ src/risp/io.rs
@@ -51,8 +51,8 @@ fn load(args: &[RVal], env: &mut REnv) -> RVal {
     match args.len() {
         1 => match &args[0] {
             _RSym(path) => env.load(&path[..]),
-            _ => RErrExpected!("(Str)", RLstArgs![args].variant()),
+            _ => RErrExpected!("(Sym)", RLstArgs![args].variant()),
         },
-        _ => RErrExpected!("(Str)", RLstArgs![args].variant()),
+        _ => RErrExpected!("(Sym)", RLstArgs![args].variant()),
     }
 }
